ffd3914ed5685f38aece834f74b0e366fcad6951
Aysu <aysu@Noutbuk-Aysu.local>
2025-05-11 12:27:42 +0400

    feat: hero section in home page

--- frontend/src/pages/Client/Home/index.tsx
+++ frontend/src/pages/Client/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import img from "../../../assets/images/home-image.jpeg";
 
 const Home = () => {
   return (
@@ -16,14 +17,26 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section>
-        <div>
-          <h3>Get home recommendations</h3>
-          <p>Sign in for a more personalized experience.</p>
-          <Link to={"/register"}>Sign in</Link>
+      <section className="bg-[#E6F6FF] ">
+        <div className="w-[52%] mx-auto py-[6rem] flex justify-between gap-2 ">
+          <div className="flex flex-col gap-2 justify-center">
+            <h3 className="text-3xl font-bold">Get home recommendations</h3>
+            <p className="text-gray-600 text-lg">
+              Sign in for a more personalized experience.
+            </p>
+            <Link
+              to={"/register"}
+              className="text-lg border-1 rounded-lg font-bold text-[#0133AA] mt-[0.5rem] py-2 w-[5.9rem] flex justify-center "
+            >
+              Sign in
+            </Link>
+          </div>
+          <div className="w-[20rem] ">
+            <img className="w-[full] h-[full] rounded-2xl " src={img} alt="" />
+          </div>
         </div>
-        <div></div>
       </section>
+      <section></section>
     </main>
   );
 };
