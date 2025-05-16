274c88fd33c2741212a9fc234f6126b821762e03
Aysu <aysu@Noutbuk-Aysu.local>
2025-05-16 19:32:58 +0400

    feat: login user

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,13 @@
-import { NavLink } from "react-router";
+import { Link, NavLink } from "react-router";
 import logo from "../../assets/images/header-logo.jpeg";
-import { House, Hotel, BookUser } from "lucide-react";
+import { House, Hotel, BookUser, User, AlignJustify } from "lucide-react";
+import { useAuth } from "@/context/AuthContext";
+import { useState } from "react";
 
 const Header = () => {
+  const { user } = useAuth();
+  const [openedMenu, setOpenedMenu] = useState(false);
+
   return (
     <header className="bg-[#FF9A1E] fixed w-full">
       <div className="w-[70%] mx-auto flex justify-between gap-2 items-center ">
@@ -43,16 +48,66 @@ const Header = () => {
             <BookUser size={17} />
             Contact
           </NavLink>
-          <NavLink
-            to={"/register"}
-            className={({ isActive }) =>
-              `border py-1 w-[5rem] rounded-lg text-center hover:bg-white hover:text-[#FF9A1E] ${
-                isActive ? "bg-white text-[#FF9A1E]" : "text-white"
-              }`
-            }
-          >
-            Sign Up
-          </NavLink>
+          <div className="flex relative flex-col gap-2 justify-start items-start">
+            <div
+              onClick={() => {
+                if (openedMenu == false) {
+                  setOpenedMenu(true);
+                } else {
+                  setOpenedMenu(false);
+                }
+              }}
+              className={`border py-1 w-[5rem] rounded-2xl flex gap-2 justify-center items-center hover:bg-white hover:text-[#FF9A1E] cursor-pointer`}
+            >
+              <AlignJustify size={17} />
+              <User />
+            </div>
+            <div
+              className={`${
+                openedMenu == false ? "hidden" : "flex"
+              } absolute bottom-[-6.8rem] w-[8rem] text-black right-0 gap-2 flex-col p-4 rounded-md z-10 bg-white shadow-lg text-[1.1rem]`}
+            >
+              {user ? (
+                <>
+                  <Link
+                    to={"/wishlist"}
+                    onClick={() => {
+                      setOpenedMenu(false);
+                    }}
+                  >
+                    Wishlist
+                  </Link>
+                  <Link
+                    to={"/login"}
+                    onClick={() => {
+                      setOpenedMenu(false);
+                    }}
+                  >
+                    Log Out
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link
+                    to={"/register"}
+                    onClick={() => {
+                      setOpenedMenu(false);
+                    }}
+                  >
+                    Sign Up
+                  </Link>
+                  <Link
+                    to={"/login"}
+                    onClick={() => {
+                      setOpenedMenu(false);
+                    }}
+                  >
+                    Log In
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </header>
