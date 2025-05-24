77c479c84d904cdae5ef83d1eae13ed470690823
Aysu <aysu@Noutbuk-Aysu.local>
2025-05-24 20:02:01 +0400

    feat: apartments page responsive

--- frontend/src/pages/Client/Apartments/index.tsx
+++ frontend/src/pages/Client/Apartments/index.tsx
@@ -13,15 +13,15 @@ const Apartments = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="w-[90%] mx-auto py-[6.1rem] flex flex-col gap-4">
-      <h1 className="font-bold text-3xl text-center">
+    <div className="w-full max-w-7xl mx-auto pt-[7rem] pb-16 px-4 sm:px-6 lg:px-8">
+      <h1 className="font-bold text-3xl text-center mb-6">
         Find Your Perfect Apartment
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
         {apartments &&
-          apartments.map((apartment: Apartment, idx: number) => {
-            return <ApartmentCard key={idx} idx={idx} apartment={apartment} />;
-          })}
+          apartments.map((apartment: Apartment, idx: number) => (
+            <ApartmentCard key={idx} idx={idx} apartment={apartment} />
+          ))}
       </div>
     </div>
   );
